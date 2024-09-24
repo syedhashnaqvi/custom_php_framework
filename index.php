e7ccaec1c2801493dd91e8c965d765cf875dcd89
syedhashnaqvi <syedhashnaqvi@gmail.com>
2024-09-24 22:38:49 +0500

    updated index file

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 use Core\Router;
 use Core\Emailer;
 include "serviceLoader.php";
-$toEmails = ["Hashmat Ali"=>"syedhashnaqvi@gmail.com","Hayat Ali"=>"alihayat452@gmail.com","Hassan Zahid"=>"hassanisavailable@gmail.com"];
-$subject = "Email Testing on dev pro!";
-// $message = "Welcome to dev pro group !!!!!";
-$template = "/emails/testmail.html";
+// $toEmails = ["Hashmat Ali"=>"syedhashnaqvi@gmail.com","Hayat Ali"=>"alihayat452@gmail.com","Hassan Zahid"=>"hassanisavailable@gmail.com"];
+// $subject = "Email Testing on dev pro!";
+// // $message = "Welcome to dev pro group !!!!!";
+// $template = "/emails/testmail.html";
 
-Emailer::send($toEmails,$subject,$template,$type="HTML");
+// Emailer::send($toEmails,$subject,$template,$type="HTML");
 new Router();
